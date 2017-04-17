7935b4f2867c2c4d34c73f25f94750f1723ec78b
avinashpalamanda <avinpalash@gmail.com>
2017-04-17 12:18:38 -0400

    first commit

--- algorithm/week_3/different_summands/different_summands.cpp
+++ algorithm/week_3/different_summands/different_summands.cpp
@@ -6,6 +6,16 @@ using std::vector;
 vector<int> optimal_summands(int n) {
   vector<int> summands;
   //write your code here
+  for(int k=n,l=1;k>0;l++){
+    if(k<2l){
+      summands.push_back(k);
+      k-=k;
+    }
+    else{
+      summands.push_back(l);
+      k=k-l;
+    }
+  }
   return summands;
 }
 
--- algorithm/week_3/largest_number/largest_number.cpp
+++ algorithm/week_3/largest_number/largest_number.cpp
@@ -6,12 +6,41 @@
 
 using std::vector;
 using std::string;
+using namespace std;
+bool IsGreaterOrEqual(string a,string b){
+  string x=a+b;
+  string y=b+a;
+  for(int i=0;i<x.size();i++){
+    if(x[i]>y[i])
+      return false;
+    else if(x[i]<y[i])
+      return true;
+    else
+      continue;
+  }
+}
+
+
 
 string largest_number(vector<string> a) {
   //write your code here
+  vector<string> res;
+  int index=0;
+  while(a.size()!=0){
+    string maxdigit=" ";
+    for(int i=0;i<a.size();i++){
+      if(IsGreaterOrEqual(maxdigit,a[i])){
+        maxdigit=a[i];
+        index=i;
+      }
+    }
+    res.push_back(maxdigit);
+    a.erase(a.begin()+index);
+  }
+
   std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
+  for (size_t i = 0; i < res.size(); i++) {
+    ret << res[i];
   }
   string result;
   ret >> result;
